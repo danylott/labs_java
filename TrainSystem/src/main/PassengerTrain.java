dde2ee5d80a5160140a2fa994e6010faac6bc1ee
Daniel <d.tuytuyshkin@gmail.com>
2020-09-06 11:07:48 +0200

    implement basic methods for PassengerTrain

--- TrainSystem/src/main/PassengerTrain.java
+++ TrainSystem/src/main/PassengerTrain.java
@@ -8,18 +8,46 @@ public class PassengerTrain extends Train {
     }
 
     public int calculateCountPassengers() {
-        return 0;
+        int countPassengers = 0;
+        for(int i = 0; i < getCarriages().size(); i++) {
+            if(getCarriages().get(i) instanceof PassengerCarriage) {
+                countPassengers += ((PassengerCarriage) getCarriages().get(i)).getPassengers().size();
+            }
+        }
+        return countPassengers;
     }
 
     public int calculateCountLuggage() {
-        return 0;
+        int countLuggage = 0;
+        for(int i = 0; i < getCarriages().size(); i++) {
+            if(getCarriages().get(i) instanceof PassengerCarriage) {
+                countLuggage += ((PassengerCarriage) getCarriages().get(i)).getLuggage().size();
+            }
+        }
+        return countLuggage;
     }
 
-    public void sortCarriagesByComfort(String type) {
-
+    public ArrayList<Carriage> sortCarriagesByComfort(String type) {
+        ArrayList<Carriage> sortedCarriages = getCarriages();
+        sortedCarriages.sort((o1, o2) -> {
+            if (o1 instanceof PassengerCarriage && o2 instanceof PassengerCarriage) {
+                return Integer.compare(((PassengerCarriage) o1).getComfort(), ((PassengerCarriage) o2).getComfort());
+            }
+            return 0;
+        });
+        return sortedCarriages;
     }
 
-    public ArrayList<Carriage> findCarriagesBYPassengerRange(int low, int high) {
-        return null;
+    public ArrayList<Carriage> findCarriagesByPassengerRange(int low, int high) {
+        ArrayList<Carriage> filteredCarriages = new ArrayList<>();
+        for(int i = 0; i < getCarriages().size(); i++) {
+            if(getCarriages().get(i) instanceof PassengerCarriage) {
+                if(low <= ((PassengerCarriage) getCarriages().get(i)).getPassengers().size()
+                        && ((PassengerCarriage) getCarriages().get(i)).getPassengers().size() <= high) {
+                    filteredCarriages.add(getCarriages().get(i));
+                }
+            }
+        }
+        return filteredCarriages;
     }
 }
